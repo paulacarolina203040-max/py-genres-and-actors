933aa90a229d5d3c300acdca59f8dbbec4c0e18f
paulacarolina203040-max <paulacarolina203040@gmail.com>
2026-09-15 19:35:16 -0300

    Genres and Actors

--- db/models.py
+++ db/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def _str_(self) -> str:
+        return f"<Genre: {self.name}>"
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def _str_(self) -> str:
+        name = f"{self.first_name}"
+        last = f"{self.last_name}"
+        return f"<Actor: {name} {last}>"
