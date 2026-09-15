a2771e4f27f587b0d103802b4c1dd8808519fe00
paulacarolina203040-max <paulacarolina203040@gmail.com>
2026-09-15 20:11:17 -0300

    Genres and actors 2

--- main.py
+++ main.py
@@ -5,38 +5,27 @@ from db.models import Actor, Genre
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genres = [
+        ("Western",),
+        ("Action",),
+        ("Drama",),
+    ]
+    for genre_tuple in genres:
+        Genre.objects.create(name=genre_tuple[0])
 
-    Actor.objects.create(
-        first_name="George",
-        last_name="Klooney"
-    )
-    Actor.objects.create(
-        first_name="Kianu",
-        last_name="Reaves"
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Keegan"
-    )
-    Actor.objects.create(
-        first_name="Will",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Jaden",
-        last_name="Smith"
-    )
-    Actor.objects.create(
-        first_name="Scarlett",
-        last_name="Johansson"
-    )
-
-    Genre.objects.filter(
-        name="Dramma"
-    ).update(name="Drama")
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name
+        )
 
     Actor.objects.filter(
         first_name="George",
@@ -59,6 +48,4 @@ def main() -> QuerySet:
         first_name="Scarlett"
     ).delete()
 
-    return Actor.objects.filter(
-        last_name="Smith"
-    ).order_by("first_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
